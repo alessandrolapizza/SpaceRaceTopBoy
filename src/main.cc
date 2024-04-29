3d8d4374ab1c41a26f99d8dd67638122cd6a19c5
Alessandro De Zen <alessandro.dezen@epfl.ch>
2024-04-29 21:51:29 +0200

    Structure

---
+++ src/main.cc
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+void setup()
+{
+  //Init. sensors
+      //Init. barometer
+      //Init. IMU
+      //Setup altitude ref.
+  
+  //Init SD card ?
+
+  //Init clock ?
+
+  //Buzzer setup complete
+}
+
+void loop()
+{
+  //Update sensors
+
+  //Write to SD Data
+
+  //Check conditions to update state and do actions of states
+}
